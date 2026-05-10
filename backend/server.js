f8ab70a2f0805b90628864ffe5fbf80b507d8115
anubhav1450 <124580419+anubhav1450@users.noreply.github.com>
2026-05-10 12:59:37 +0530

    Update validation and UI improvements

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import app from "./src/app.js";
+
 import connectDB from "./src/db/db.js";
+
 import authRoutes from "./src/routes/authRoutes.js";
 
+import storyRoutes from "./src/routes/storyRoutes.js";
 
 connectDB();
+
 app.use("/api/auth", authRoutes);
 
+app.use("/api/stories", storyRoutes);
 
-app.listen(3000, () => {
-  console.log("Server is live");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+  console.log(`Server is live on port ${PORT}`);
+
 });
